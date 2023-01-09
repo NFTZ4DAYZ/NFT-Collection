a1525c47a2bec6aa68b2b5705623c83b4d1fa4e3
Dexter Matis <Dexterwray@proton.me>
2023-01-09 17:16:43 +0000

    dex

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xbfE0c5b9e95715678a7Eb214f9D96eA6155c3E03";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
